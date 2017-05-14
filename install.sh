8bc252e09338f6294a505a4dc25f0daab89e949d
Andro Misa <andro260@gmail.com>
2017-05-14 20:22:41 +0800

    Updated install file.

--- install.sh
+++ install.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 
-#Install zsh
-sudo apt-get install zsh
-sudo apt-get install git-core
+# Install required apps
+sudo apt-get install -yq 
+	git vim \
+	tmux \
+	zsh \
+	git-core \
+	exuberant-ctags \
+	silversearcher-ag
 
-#Install oh-my-zsh
+# Install oh-my-zsh
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
 chsh -s `which zsh`
 
-#Install Python pip
-sudo apt-get install python-pip
+# Install Python pip
+sudo apt-get install python-pip -yq
 sudo pip install powerline-status
 git clone https://github.com/powerline/fonts.git && cd fonts && sh ./install.sh
 
-#Install exuberant-ctags
-sudo apt-get install exuberant-ctags
+# Install vundle
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-#Install silver searcher
-sudo apt-get install silversearcher-ag
-
-#Reboot
+# Reboot
 sudo reboot
 
