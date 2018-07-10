a066fdfe6378b377f02b35d7b4f2837fdbba8546
Andro Misa <andro260@gmail.com>
2018-07-10 04:19:48 +0000

    Commented out install.sh script

--- install.sh
+++ install.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 # Install required apps
-sudo apt-get install git
-	vim \
-	tmux \
-	zsh \
-	git-core \
-	exuberant-ctags \
-	silversearcher-ag -yq
+#sudo apt-get install git
+	#vim \
+	#tmux \
+	#zsh \
+	#git-core \
+	#exuberant-ctags \
+	#silversearcher-ag -yq
 
 # Install oh-my-zsh
-wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
-chsh -s `which zsh`
+#wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+#chsh -s `which zsh`
 
 # Install Python pip
-sudo apt-get install python-pip -yq
-sudo pip install powerline-status
-git clone https://github.com/powerline/fonts.git && cd fonts && sh ./install.sh
+#sudo apt-get install python-pip -yq
+#sudo pip install powerline-status
+#git clone https://github.com/powerline/fonts.git && cd fonts && sh ./install.sh
 
 # Install vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 # Reboot
-sudo reboot
+#sudo reboot
 
